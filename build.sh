ffd64ca5b09a3f6a05533e057f9fa2e5c60370d7
frasator <frasator@gmail.com>
2016-06-07 12:24:21 +0200

    Removed jsorolla dependecy, Save session now uses file browser.

--- build.sh
+++ build.sh
@@ -1,58 +1,29 @@
 #!/bin/sh
 NAME="cell-maps"
+ELEMENT="cell-maps-element"
 BP=build
 
 rm -rf $BP
 mkdir -p $BP
-mkdir -p $BP/tmp
-mkdir -p $BP/fonts
 mkdir -p $BP/fontawesome
-mkdir -p $BP/images
-mkdir -p $BP/css
+mkdir -p $BP/webcomponentsjs
 
-vulcanize \
-    --inline-scripts \
-    --inline-css \
-    --strip-comments \
-    src/$NAME-element.html > $BP/tmp/build.html
+vulcanize --inline-scripts --inline-css --strip-comments $ELEMENT.html > $BP/$ELEMENT.html
 
-crisper \
-    --source $BP/tmp/build.html \
-    --html $BP/tmp/$NAME-element.html \
-    --js $BP/tmp/$NAME.js
-
-uglifyjs $BP/tmp/$NAME.js > $BP/tmp/$NAME.min.js
-sed -i s@$NAME.js@$NAME.min.js@g $BP/tmp/$NAME-element.html
-rm -rf $BP/tmp/build.html
-
-cp COPYING $BP/
-cp README.md $BP/
-
-mv $BP/tmp/$NAME-element.html $BP/
-mv $BP/tmp/$NAME.js $BP/
-mv $BP/tmp/$NAME.min.js $BP/
-cp -r conf $BP/
-cp -r example-files $BP/
 cp -r $NAME-index.html $BP/index.html
+cp -r conf/ $BP/
+cp -r example-files $BP/
 
-#
-# fix index.html paths
-#
-sed -i s@lib/jsorolla/styles/@@g $BP/index.html
-cp -r lib/jsorolla/styles/fonts/* $BP/fonts/
-
-sed -i s@bower_components/@@g $BP/index.html
+cp -r bower_components/stevia-elements/fonts/ $BP/
+cp -r bower_components/stevia-elements/css/ $BP/
 cp -r bower_components/fontawesome/css $BP/fontawesome/
 cp -r bower_components/fontawesome/fonts $BP/fontawesome/
-cp -r bower_components/webcomponentsjs $BP/
+cp -r bower_components/webcomponentsjs/*.min.js $BP/webcomponentsjs/
 
-sed -i s@lib/jsorolla/src/lib/components/@css/@g $BP/index.html
-cp -r lib/jsorolla/src/lib/components/jso-global.css $BP/css/
-cp -r lib/jsorolla/src/lib/components/jso-form.css $BP/css/
-cp -r lib/jsorolla/src/lib/components/jso-dropdown.css $BP/css/
 
-sed -i s@src/@@g $BP/index.html
+#
+# fix index.html css paths
+#
+sed -i s@'bower_components/stevia-elements/'@@g $BP/index.html
+sed -i s@'bower_components/'@@g $BP/index.html
 ## end fix paths
-
-
-rm -rf $BP/tmp
--- conf/config.js
+++ conf/config.js
@@ -4,9 +4,6 @@
  **/
 
 // Default
-OPENCGA_VERSION = "v1";
-OPENCGA_HOST = "http://ws.babelomics.org/opencga";
-
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
 
@@ -15,8 +12,11 @@ CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
 CELLBASE_LOG = true;
 
-OPENCGA_VERSION = "v1";
-OPENCGA_HOST = "http://ws1.babelomics.org/opencga-0.7";
-OPENCGA_LOG = true;
 
-STUDY_NAME = "CHECK_THIS";
+STEVIA_MANAGER_LOG = true;
+STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
+// STEVIA_SERVER_HOST = 'http://stvtest.babelomics.org';
+// STEVIA_SERVER_HOST = 'http://localhost:5555';
+
+STEVIA_SHOW_JOB_ERROR_FILES = true;
+UPLOAD_FILES_WITH_ERRORS = true;
